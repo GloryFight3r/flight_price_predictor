9a2ec10bc0e48f729d33d5758bd5f1ccee71e477
Borislav Semerdzhiev <borislavsemerdzhiev.02@gmail.com>
2023-08-06 14:43:12 +0300

    Graphs flight prices

--- src/scraper.py
+++ src/scraper.py
@@ -1,75 +1,91 @@
-from bs4 import BeautifulSoup as bs
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from logging import exception
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
 import re
+import os
+from datetime import date, timedelta
+import pandas as pd
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-import csv
-
-
-# scrape destinations
 
 url: str = "https://www.google.com/travel/flights?tfs=CBwQARoaEgoyMDIzLTEwLTA4agwIAhIIL20vMGZ0anhAAUgBcAGCAQsI____________AZgBAg&hl=en-GB&curr=EUR"
 
 driver = webdriver.Chrome()
-
-driver.get(url)
-
+current_day = date.today()
 patterns = [r"(\d+) days ago - €(\d+)", r"(\d+) day ago - €(\d+)", "Today - €(\d+)"]
 
-with open('../data/plane_logs', 'w') as f:
-    writer = csv.writer(f)
-    writer.writerow(["From", "To", "Days Ago", "Price"])
-    departs_from:str = "Paris"
-    arrives_at:str = "Eindhoven"
-    date:str = "8 Aug"
+IDLE_TIME = 1.3
 
+destinations = [
+    ("Sofia", "Eindhoven", "23 Aug"), 
+    ("Canada", "Rome", "23 Aug"),
+    ("Canada", "Rome", "2 Sep")
+]
+
+destinations = [("Sofia", "Eindhoven", "{} Aug".format(i)) for i in range(6, 32)]
+
+for departs_from, arrives_at, date in destinations:
+    cur_file_name = '{}.csv'.format(date)
+    cur_dir = '../data/{}-{}/'.format(departs_from, arrives_at)
+    
+    df = pd.DataFrame({
+        "Price at Date":[],
+        "Price in EUR":[]
+    })
+    try:
+        if cur_file_name in os.listdir(cur_dir):
+            df = pd.read_csv(cur_dir + cur_file_name)
+    except FileNotFoundError as e:
+        os.mkdir(cur_dir)
+        print("No such dir")
+    driver.get(url)
+    sleep(IDLE_TIME)
     try:
         # gets rid of the google accept all window
-        button = driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/div[1]/form[2]/div/div/button')
-        button.click()
+        buttons = driver.find_elements(By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/div[1]/form[2]/div/div/button')
+        if len(buttons) > 0:
+            buttons[0].click()
         
-        sleep(1)
+        sleep(IDLE_TIME)
         # enter departure
         departs_field = driver.find_elements(By.TAG_NAME, 'input')[0]#lJj3Hd')#//*[@id="i21"]/div[6]/div[2]/div[2]')#/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[6]/div[2]/div[2]/div[1]/div/input')
         departs_field.clear()
         departs_field.send_keys(departs_from)
-        sleep(1)
+        sleep(IDLE_TIME)
         departs_from_listbox = driver.find_element(By.CLASS_NAME, 'DFGgtd')#//*[@id="i21"]/div[4]/div/div/div[1]/div/div/input')
         departs_from_listbox.find_elements(By.TAG_NAME, 'li')[0].click()
         
-        sleep(1)
+        sleep(IDLE_TIME)
         # enter arrival
         arrives_field = driver.find_elements(By.TAG_NAME, 'input')[2]#//*[@id="i21"]/div[4]/div/div/div[1]/div/div/input')
         arrives_field.clear()
         arrives_field.send_keys(arrives_at)
-        sleep(1)
+        sleep(IDLE_TIME)
         arrives_at_listbox = driver.find_element(By.CLASS_NAME, 'DFGgtd')#//*[@id="i21"]/div[4]/div/div/div[1]/div/div/input')
         arrives_at_listbox.find_elements(By.TAG_NAME, 'li')[0].click()
 
-        sleep(1)
+        sleep(IDLE_TIME)
 
         # input date
         date_field = driver.find_elements(By.TAG_NAME, 'input')[4]#//*[@id="i21"]/div[4]/div/div/div[1]/div/div/input')
         date_field.clear()
+        for i in range(10):
+            date_field.send_keys(Keys.BACKSPACE)
         date_field.send_keys(date)
-        sleep(1)
+        sleep(IDLE_TIME)
 
         # click on button to search
         search_button = driver.find_element(By.CSS_SELECTOR, '[aria-label=Search]')
         search_button.click()
-        sleep(3)
+        sleep(IDLE_TIME)
         driver.find_element(By.XPATH, "//div[text()='View price history']").click()
-        sleep(1)
+        sleep(IDLE_TIME)
 
         # click on grid
         button = driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[2]/div[4]/div')
         button.click()
  
-        sleep(1)
+        sleep(IDLE_TIME)
 
         prices = driver.find_elements(By.CLASS_NAME, 'pKrx3d-JNdkSc')
         for x in prices:
@@ -95,10 +111,11 @@ with open('../data/plane_logs', 'w') as f:
                     break
             # save the data
             assert has_matched
-            writer.writerow([departs_from, arrives_at, day, price])
-        sleep(0.1)
-    except Exception:
-        print("AS")
-
+            df.loc[len(df)] = [(current_day - timedelta(days=day)).strftime("%Y/%m/%d"), price]
+    except Exception as e:
+        destinations.append((departs_from, arrives_at, date))
+        print(e)
+    df.drop_duplicates(subset=['Price at Date'], keep="last", inplace=True)
+    df.to_csv(cur_dir + cur_file_name, index=False)
 driver.quit()
 
